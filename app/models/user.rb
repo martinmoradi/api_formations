9d75d687d8027e1118ea9b97d799c243f84f355a
Martin Moradi <moradi.martin@gmail.com>
2021-03-08 15:12:50 +0100

    auth ok

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+end
