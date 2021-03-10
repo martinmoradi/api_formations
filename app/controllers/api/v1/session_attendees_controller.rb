45b3ca64f25d58cd85fafd60a0adc4fae7ea82f1
Martin Moradi <moradi.martin@gmail.com>
2021-03-10 14:27:14 +0100

    typo

--- app/controllers/api/v1/session_attendees_controller.rb
+++ app/controllers/api/v1/session_attendees_controller.rb
@@ -1,44 +1,44 @@
-  class Api::V1::SessionAttendeesController < Api::BaseController
-    before_action :set_session_attendee, only: %i[show update destroy]
-    before_action :authenticate_user!, except: %i[index]
+class Api::V1::SessionAttendeesController < Api::BaseController
+  before_action :set_session_attendee, only: %i[show update destroy]
+  before_action :authenticate_user!, except: %i[index]
 
-    def index
-      @session_attendees = SessionAttendee.all
-      render json: CourseSessionSerializer.new(@session_attendees)
-    end
+  def index
+    @session_attendees = SessionAttendee.all
+    render json: CourseSessionSerializer.new(@session_attendees)
+  end
 
-    def show
-      render json: @session_attendee
-    end
+  def show
+    render json: @session_attendee
+  end
 
-    def create
-      @session_attendee = SessionAttendees.new(session_attendee_params)
-      if @session_attendee.save
-        render json: @session_attendee, status: :created, api_v1_session_attendees_url(@session_attendee)
-      else
-        render json: @session_attendee.errors, status: :unprocessable_entity
-      end
-    end
-
-    def update
-      if @session_attendee.update(session_attendee_params)
-        render json: @session_attendee
-      else
-        render json: @session_attendee.errors, status: :unprocessable_entity
-      end
-    end
-
-    def destroy
-      @session_attendee.delete
-    end
-
-    private
-
-    def set_session_attendee
-      @session_attendee = SessionAttendee.find(params[:id])
-    end
-
-    def session_attendee_params
-      params.require(:session_attendee).permit(:assessment, :student_id, :session_id)
+  def create
+    @session_attendee = SessionAttendees.new(session_attendee_params)
+    if @session_attendee.save
+      render json: @session_attendee, status: :created, location: api_v1_session_attendees_url(@session_attendee)
+    else
+      render json: @session_attendee.errors, status: :unprocessable_entity
     end
   end
+
+  def update
+    if @session_attendee.update(session_attendee_params)
+      render json: @session_attendee
+    else
+      render json: @session_attendee.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @session_attendee.delete
+  end
+
+  private
+
+  def set_session_attendee
+    @session_attendee = SessionAttendee.find(params[:id])
+  end
+
+  def session_attendee_params
+    params.require(:session_attendee).permit(:assessment, :student_id, :session_id)
+  end
+end
