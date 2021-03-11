54df7e44de1142c2d1e2c4e86c7498a0c2fc6ab8
Martin Moradi <moradi.martin@gmail.com>
2021-03-11 22:27:58 +0100

    nullify associations on user delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-  
-  has_many :session_attendees, dependant: :destroy
+  has_many :session_attendees, dependant: :nullify
   has_many :course_sessions, through: :session_attendees, dependent: :nullify
 
   validates :email,
